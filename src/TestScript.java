f657a07244214e9c1799eb56662514490097d6eb
nathanD <nathandevery@gmail.com>
2023-04-30 14:13:22 +1200

    initial using dreambot

---
+++ src/TestScript.java
@@ -0,0 +1,17 @@
+import org.dreambot.api.script.AbstractScript;
+import org.dreambot.api.script.Category;
+import org.dreambot.api.script.ScriptManifest;
+import org.dreambot.api.utilities.Logger;
+
+@ScriptManifest(name = "Script Name", description = "My script description!", author = "Developer Name",
+        version = 1.0, category = Category.WOODCUTTING, image = "")
+public class TestScript extends AbstractScript {
+
+    @Override
+    public int onLoop() {
+        Logger.log("My first script!");
+
+        return 0;
+    }
+
+}
